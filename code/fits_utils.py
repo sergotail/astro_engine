0853439030169dae65ab001248e0893edc173be9
sergotail <sergotail@mail.ru>
2017-05-13 18:32:26 +0300

    just another commit

--- code/fits_utils.py
+++ code/fits_utils.py
@@ -1,7 +1,6 @@
-from apex.io import *
-from apex.astrometry import Simple_Astrometry
-
 def edit_header_coords_info(filename, ra_str, dec_str, scale_sec_pix, frame_rotation=0, flip=0, ra_dec_delim=' '):
+    from apex.io import *
+    from apex.astrometry import Simple_Astrometry
     ra_list = [float(ra) for ra in ra_str.split(ra_dec_delim)]
     dec_list = [float(dec) for dec in dec_str.split(ra_dec_delim)]
     # TODO: check if ra and dec lists are correct
@@ -14,3 +13,16 @@ def edit_header_coords_info(filename, ra_str, dec_str, scale_sec_pix, frame_rota
     im.wcs = Simple_Astrometry(ra, dec, im.width/2.0, im.height/2.0,
                                (2*flip - 1)*scale, scale, frame_rotation)
     imwrite(im, im.filename)
+
+# function returns list of filenames with specified format in directory
+# TODO: support param "return full pathname" (False by default)
+# TODO: implement separate functions for tech frames search (needed for tech frames preprocessing step)
+# TODO: define naming convention for super tech frames
+# TODO: support multiply files_format values
+# TODO: support os.walk (nested dirs) flag
+def get_fits_images_from_dir(directory, files_format='fit', exact_pattern=True):
+    import os
+    from re import search, IGNORECASE
+    regex_end = '$' if exact_pattern else ''
+    files_format += regex_end
+    return [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)) and search(files_format, f, IGNORECASE) is not None]
